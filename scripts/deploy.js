174105ae8f133c0448bef890e6f7d876656f1727
ATHARVA-TIRKHUNDE <85235935+ATHARVA-TIRKHUNDE@users.noreply.github.com>
2023-10-18 16:55:12 +0530

    Update deploy.js
    the functions used in deploy.js while making the project are depricated in the new version so the code provided is the alternative for those who are building the project with new resources and version

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,12 +1,32 @@
+
 const hre = require("hardhat");
 
 async function main() {
   const Upload = await hre.ethers.getContractFactory("Upload");
   const upload = await Upload.deploy();
-
   await upload.deployed();
-
   console.log("Library deployed to:", upload.address);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
+
+
+// If you are geting this error with above code then try using the code below
+// TypeError: upload.deployed is not a function
+//     at main (D:\Blockchain Projects\Decentralize-Drive\scripts\deploy.js:7:16)
+//     at processTicksAndRejections (node:internal/process/task_queues:95:5)
+
+const hre = require("hardhat");
+
+async function main() {
+  
+  const upload = await hre.ethers.deployContract("Upload");
+  await upload.waitForDeployment();
+
+  console.log("Library deployed to:", upload.target);
 }
 
 main().catch((error) => {
